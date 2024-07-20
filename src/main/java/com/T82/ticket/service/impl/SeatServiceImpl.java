e252028ee88d53b378310f49a47bce3bf6bf7626
jungryuel <zldrktduf0927@naver.com>
2024-07-20 21:49:51 +0900

    Feat: redis의 radssion을 이용한 분산락 적용 기능

--- src/main/java/com/T82/ticket/service/impl/SeatServiceImpl.java
+++ src/main/java/com/T82/ticket/service/impl/SeatServiceImpl.java
@@ -12,10 +12,13 @@ import com.T82.ticket.global.domain.repository.SectionRepository;
 import com.T82.ticket.service.SeatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +28,7 @@ public class SeatServiceImpl implements SeatService {
     private final SectionRepository sectionRepository;
     private final SeatRepository seatRepository;
     private final ChoiceSeatRepository choiceSeatRepository;
+    private final RedissonClient redissonClient;
     @Override
     public List<AvailableSeatsResponseDto> getAvailableSeats(Long eventId) {
         List<Seat> seats = sectionRepository.findAllSeatsByEventId(eventId);
@@ -33,19 +37,36 @@ public class SeatServiceImpl implements SeatService {
     }
 
     @Override
-    @Transactional
     public void choiceSeats(List<ChoiceSeatsRequest> req, String userId) {
-
-        req.forEach(dto->{
-            //좌석을 찾고
-            Seat seat = seatRepository.findById(dto.seatId()).orElseThrow(SeatNotFoundException::new);
-            // 좌석이 이미 선택 됐을 경우
-            if(seat.getIsChoicing()) throw new SeatAlreadyChosenException();
-            //선택중으로 바꾸고
-            seat.setIsChoicing(true);
-            //pending 테이블에 정보를 저장
-            choiceSeatRepository.save(dto.toEntity(userId));
-        });
+        req.forEach(dto -> lockAndProcessSeat(dto, userId));
     }
 
+    public void lockAndProcessSeat(ChoiceSeatsRequest choiceSeatsRequest, String userId) {
+        String lockKey = "lock:seat:" + choiceSeatsRequest.seatId();
+        RLock lock = redissonClient.getLock(lockKey);
+
+        boolean isLocked = false;
+        try {
+            // 락 획득 시도
+            isLocked = lock.tryLock( 5, 300, TimeUnit.SECONDS);
+            if (isLocked) {
+                // 좌석을 찾고
+                Seat seat = seatRepository.findById(choiceSeatsRequest.seatId()).orElseThrow(SeatNotFoundException::new);
+                // 좌석이 이미 선택됐을 경우
+                if (seat.getIsChoicing()) throw new SeatAlreadyChosenException();
+                // 선택중으로 바꾸고
+                seat.setIsChoicing(true);
+                // pending 테이블에 정보를 저장
+                choiceSeatRepository.save(choiceSeatsRequest.toEntity(userId));
+            } else {
+                throw new RuntimeException("Could not acquire lock for key: " + lockKey);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted while trying to acquire lock for key: " + lockKey, e);
+        } finally {
+            if (isLocked) {
+                lock.unlock();
+            }
+        }
+    }
 }
