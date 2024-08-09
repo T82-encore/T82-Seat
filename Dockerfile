59241c3d4d6ec307f9717f4a15cd3736a9aae32f
jungryuel <90687921+jungryuel@users.noreply.github.com>
2024-08-09 18:27:59 +0900

    Feature grpc (#48)
    * [feat] seat -> ticket grpc 추가
    
    * FIX: GRPC 수정
    
    ---------
    
    Co-authored-by: HuiSeokJeong <tobysug123@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN ["java" ,"-version"]
 
 #docker run 시 실행할 명령어
 CMD ["java","-jar","-Dspring.profiles.active=${profiles}","app.jar"]
-EXPOSE 8080
+EXPOSE 8080 9090
---
+++ src/main/java/com/T82/ticket/config/grpc/GrpcServer.java
@@ -0,0 +1,65 @@
+package com.T82.ticket.config.grpc;
+
+import com.T82.common_exception.exception.seat.SeatNotFoundException;
+import com.T82.common_exception.exception.seat.SectionNotFoundException;
+import com.T82.ticket.global.domain.entity.Seat;
+import com.T82.ticket.global.domain.entity.Section;
+import com.T82.ticket.global.domain.repository.SeatRepository;
+import com.T82.ticket.global.domain.repository.SectionRepository;
+import io.grpc.stub.StreamObserver;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.autoconfigure.GrpcServerSecurityAutoConfiguration;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.t82.seat.lib.SeatDetailRequest;
+import org.t82.seat.lib.SeatDetailResponse;
+import org.t82.seat.lib.SeatGrpc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@GrpcService
+@RequiredArgsConstructor
+@Slf4j
+@EnableAutoConfiguration(exclude = {GrpcServerSecurityAutoConfiguration.class})
+public class GrpcServer extends SeatGrpc.SeatImplBase{
+    private final GrpcUtil grpcUtil;
+    private final SectionRepository sectionRepository;
+    private final SeatRepository seatRepository;
+
+    @Override
+    public void getSeatDetail(SeatDetailRequest request, StreamObserver<SeatDetailResponse> responseObserver) {
+        List<Seat> seats = new ArrayList<>();
+        request.getSeatIdList().forEach(seatId -> {
+            Seat seat = seatRepository.findById(seatId)
+                    .orElseThrow(SeatNotFoundException::new);
+            log.info("seatId" + seat.getSeatId());
+
+            responseObserver.onNext(
+                    SeatDetailResponse.newBuilder()
+                            .setId(seatId)
+                            .setSection(seat.getSection().getName())
+                            .setRowNum(seat.getRowNum())
+                            .setColNum(seat.getColNum())
+                            .build()
+            );
+            seats.add(seat);
+        });
+        responseObserver.onCompleted();
+        complete(seats);
+    }
+
+    @Transactional
+    public void complete(List<Seat> seats) {
+        seats.forEach(seat -> {
+            Seat.SeatBook(seat);
+
+            Section section = sectionRepository.findById(seat.getSection().getSectionId())
+                    .orElseThrow(SectionNotFoundException:: new);
+
+            Section.DecreaseInSectionSeats(section);
+        });
+    }
+}
